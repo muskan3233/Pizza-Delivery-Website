d6738f2337f8755dd7c948e6a1bb45ade8f8c329
Rehmat Shah <muskanjee2021r@gmail.com>
2023-12-16 00:21:00 +0530

    1st commit

---
+++ partials/_footer.php
@@ -0,0 +1,3 @@
+<div class="footer container-fluid bg-dark text-light">
+    <p class="text-center py-2 mb-0">Copyright © 2023 Designed by <a href="https://github.com/muskan3233" target="_blank" rel="noopener noreferrer">Rehmatunnisa - 21BCS9723</a></p>
+</div>
